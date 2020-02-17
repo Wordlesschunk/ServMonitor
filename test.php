3eeb40d559b7e5e58d1b2cc912d5771ad1aaaaea
Dylan Jennings <dylan.jennings077@gmail.com>
2020-02-17 02:16:06 +0000

    Current base code
    Uploaded current base framework

---
+++ test.php
@@ -0,0 +1,13 @@
+<script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
+<script>
+$(document).ready(function(){
+setInterval(function(){
+$("#screen").load('inc/serv.stat.php')
+}, 1000);
+});
+</script>
+
+<div id="screen">
+<?php include_once("inc/serv.stat.php"); ?>
+</div>
+
