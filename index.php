6916a71570dc5319d2d2df511bdffda89b4c0857
Dylan Jennings <dylan.jennings077@gmail.com>
2020-02-17 02:21:40 +0000

    Added Reload Script
    Added the reload scrip to reload the processing block & altered formatting.

--- index.php
+++ index.php
@@ -11,16 +11,28 @@
     <title>SM 0.1</title>
   </head>
 
+<!-- Script to reload div element -->
+<script src="https://code.jquery.com/jquery-2.1.1.min.js" type="text/javascript"></script>
+<script>
+$(document).ready(function(){
+setInterval(function(){
+$("#processing").load('inc/serv.stat.php')
+}, 1000);
+});
+</script>
+
+
   <body>
-
+        <!-- Data collection & processing -->
+        <div id="processing">
         <?php include_once("inc/serv.stat.php"); ?>
-
+        </div>
+    
+    
     <h1>ServMonitor | BETA 0.0.1</h1>
-
     <div class="container">
   <div class="row">
     <div class="col">
-
     <h2> % Disk Used:</h2>
     <div class="progress" style="height: 35px;">
         <div class="progress-bar" role="progressbar" style="width: <?php echo $dp; ?>%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"><h4><?php echo $dp; ?>%</h4></div>
@@ -31,13 +43,10 @@
     <span class="badge badge-danger"><?php echo "$df of $dt Free"; ?></span>
     </h2>
 
-
     </div>
   </div>
 </div>
 
-
-   
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
